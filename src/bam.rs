a43213208107f63ab8504012ffabcd0b5e350195
Ehsan Haghshenas <ehsan@oceangenomics.com>
2021-09-21 23:06:50 -0400

    make sure single-cell tags are passed to the output

--- src/bam.rs
+++ src/bam.rs
@@ -12,7 +12,7 @@ use crate::convert;
 extern crate fnv;
 use fnv::FnvHashMap;
 
-use log::{debug};
+use log::{debug, error};
 
 // pub fn bam2bam(input_bam_filename: &String, 
 //                output_bam_filename: &String,
@@ -341,7 +341,6 @@ pub fn bam2bam(input_bam_filename: &String,
         } else {
             if mate_wanted {
                 // this is the second read in pair... PROCESS!
-                // println!("processing2");
                 let txp_records = convert::convert_paired_end(&first_record,
                                                                         &record,
                                                                         header_view,
@@ -349,21 +348,99 @@ pub fn bam2bam(input_bam_filename: &String,
                                                                         txp_lengths,
                                                                         trees,
                                                                         max_softlen);
-                // println!("vector length: {}", txp_records.len());
                 for txp_rec in txp_records.iter() {
                     output_bam.write(txp_rec).unwrap();
-                    // println!("converted {}", String::from_utf8(txp_rec.qname().to_vec()).unwrap());
                 }
                 mate_wanted = false;
             } else {
-                // println!("processing1");
                 // this is the first read in pair... save and wait for the second read in the pair
                 mate_wanted = true;
                 first_record = record;
             }
         }
-        // if n == 4 {
-        //     break;
-        // }
+    }
+}
+
+pub fn bam2bam_tags(input_bam_filename: &String, 
+                   output_bam_filename: &String,
+                   transcripts: &Vec<String>,
+                   txp_lengths: &Vec<i32>,
+                   trees: &FnvHashMap::<String, COITree<ExonNode, u32>>,
+                   threads_count: &usize,
+                   max_softlen: &usize, 
+                   required_tags: &Vec<&str>) {
+    let mut input_bam = Reader::from_path(input_bam_filename).unwrap();
+    // let bam_records = input_bam.records();
+
+    // let header_ = Header::from_template(input_bam.header());
+    // let header_text = String::from_utf8(header_.to_bytes().to_owned()).unwrap().;
+
+    let input_bam_header = Reader::from_path(input_bam_filename).unwrap();
+    let header_view = input_bam_header.header();
+    
+    let mut new_header = Header::new();
+    // new_header.push_record(header::HeaderRecord::new(b"HD").push_tag(b"VN", &"1.4"));
+
+    let mut counter = 0;
+    for tid in transcripts.iter() {
+        let txp_len = txp_lengths[counter];
+        new_header.push_record(header::HeaderRecord::new(b"SQ").push_tag(b"SN", &tid).push_tag(b"LN", &txp_len));
+        counter += 1;
+    }
+
+    let mut output_bam = Writer::from_path(output_bam_filename, &new_header, Format::Bam).unwrap();
+
+    if *threads_count >= 2 {
+        let threads_count_half = threads_count / 2;
+        println!("thread count: {}", threads_count_half);
+        input_bam.set_threads(threads_count_half).expect("Failed to set number of BAM reading threads.");
+        output_bam.set_threads(threads_count_half).expect("Failed to set number of BAM writing threads.");
+    }
+
+    let mut n = 0;
+    let mut mate_wanted = false;
+    let mut first_record: record::Record = record::Record::new();
+    for rec in input_bam.records() {
+        n = n + 1;
+        let record = rec.unwrap();
+        let qname = String::from_utf8(record.qname().to_vec()).unwrap();
+        for tag in required_tags.iter() {
+            if record.aux(tag.as_bytes()).is_err() {
+                error!("Could not find {} tag for read {}", tag, qname);
+                panic!("Some required tags do not exist!");
+            }
+        }
+
+        debug!("qname: {}", qname);
+        if !record.is_paired() || record.is_mate_unmapped() {
+            let txp_records = convert::convert_single_end(&record, 
+                                           header_view,
+                                           transcripts,
+                                           trees,
+                                           max_softlen);
+            for txp_rec in txp_records.iter() {
+                output_bam.write(txp_rec).unwrap();
+            }
+            mate_wanted = false;
+        } else {
+            if mate_wanted {
+                // this is the second read in pair... PROCESS!
+                let txp_records = convert::convert_paired_end(&first_record,
+                                                                        &record,
+                                                                        header_view,
+                                                                        transcripts,
+                                                                        txp_lengths,
+                                                                        trees,
+                                                                        max_softlen);
+                for txp_rec in txp_records.iter() {
+                    output_bam.write(txp_rec).unwrap();
+                }
+                mate_wanted = false;
+            } else {
+                // this is the first read in pair... save and wait for the second read in the pair
+                mate_wanted = true;
+                first_record = record;
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -40,9 +40,10 @@ fn main() {
         .about("Convert alignment of single-cell RNA-Seq reads against genome to alignment against transcriptome.")
         .arg(Arg::from("-b, --bam=<bam-file> 'input SAM/BAM file'"))
         .arg(Arg::from("-g, --gtf=<gtf-file> 'input gtf/gff file'"))
-        .arg(Arg::from("-o, --out=<output-file> 'output BAM file'"))
+        .arg(Arg::from("-o, --out=<output-file> 'output file name'"))
         .arg(Arg::from("-t, --threads 'Number of threads for the processing bam files.'").default_value(&default_num_threads))
-        .arg(Arg::from("-s, --max-softlen 'Max allowed sofclip length allowed.'").default_value(&default_max_softlen));
+        .arg(Arg::from("-s, --max-softlen 'Max allowed sofclip length allowed.'").default_value(&default_max_softlen))
+        .arg(Arg::from("-c, --corrected-tags 'Output error-corrected cell barcode and UMI.'"));
 
     let opts = App::new("mudskipper")
         .setting(AppSettings::SubcommandRequiredElseHelp)
@@ -80,7 +81,41 @@ fn main() {
         } else {
             bam::bam2bam(&bam_file_in, &out_file, &transcripts, &txp_lengths, &trees, &threads_count, &max_softlen);
         }
-    } else if let Some(ref _t) = opts.subcommand_matches("sc") {
-        println!("Support for single-cell bam files not added yet!")
+    } else if let Some(ref t) = opts.subcommand_matches("sc") {
+        let bam_file_in: String = t.value_of_t("bam").unwrap();
+        let ann_file_adr: String = t.value_of_t("gtf").unwrap();
+        let out_file: String = t.value_of_t("out").unwrap();
+        let threads_count: usize = t.value_of_t("threads").unwrap();
+        let max_softlen: usize = t.value_of_t("max-softlen").unwrap();
+        // 
+        let mut transcripts_map: HashMap<String, i32> = HashMap::new();
+        let mut transcripts: Vec<String> = Vec::new();
+        let mut txp_lengths: Vec<i32> = Vec::new();
+        let trees = if std::fs::metadata("parsed_gtf.exon").is_ok() {
+            annotation::load_tree(&mut transcripts_map,
+                                   &mut transcripts,
+                                   &mut &mut txp_lengths).expect("cannot load the tree!")
+        } else {
+            annotation::build_tree(&ann_file_adr, 
+                &mut transcripts_map,
+                &mut transcripts,
+                &mut txp_lengths).expect("cannot build the tree!")
+        };
+
+        let required_tags: Vec<&str>;
+        if t.is_present("corrected-tags") {
+            required_tags = vec!["CB", "UB"];
+        } else {
+            required_tags = vec!["CR", "UR"];
+        }
+        bam::bam2bam_tags(&bam_file_in,
+                          &out_file,
+                          &transcripts,
+                          &txp_lengths,
+                          &trees,
+                          &threads_count,
+                          &max_softlen,
+                          &required_tags);
+        // println!("{:?}", required_tags);
     }
 }
